cfb0f3ac446e408d51a0e94439adc7f0cdbbc946
Lmanzanel <Lmanzanel@venturihnos.local>
2023-03-03 11:25:52 -0300

    ok

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styles: []
 })
 export class RegisterComponent implements OnInit {
+  
+  registroForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit() {
+    this.registroForm = this.fb.group({
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+
+
+  crearUsuario(){
+    
   }
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
-};
+  production: false,
+  firebase: {
+      apiKey: "AIzaSyCZCRceAszJL08iGLz_x-ObiGn2GIz7r80",
+      authDomain: "ingresoegresoapp-90100.firebaseapp.com",
+      projectId: "ingresoegresoapp-90100",
+      storageBucket: "ingresoegresoapp-90100.appspot.com",
+      messagingSenderId: "693987825245",
+      appId: "1:693987825245:web:601bf15c45901fc3a39fb4"
+  }
+}
